710a4b4dce2b21b5abc1b93f9fc0aedd28c44bcf
Vinicios Wentz <vinicios.wentz@icloud.com>
2024-04-03 12:15:19 -0300

    Initial Commit

---
+++ Piazza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piazza
+//
+//  Created by Vinicio Wentz on 03/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
